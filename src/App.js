b3bb6b638601793929650861de61300428f84bf2
bobbyjsx <ezealagodswill@gmail.com>
2023-02-11 16:41:11 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {Data} from './components/Data'
 import { FiMinus, FiPlus, FiArrowUp} from 'react-icons/fi'
 import './App.css'
 
 const App = () => {
   const [isClicked, setIsClicked] = useState(false)
+  // 
+  const [display, setDisplay] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDisplay(true);
+    }, 2000);
+  }, []);
+  // 
 
   const handleToggle = index =>{
     if(isClicked === index){
-      return setIsClicked(null)
+      
+      return setTimeout(setIsClicked(null), 20000)
     }else{
       setIsClicked(index)
     }
@@ -16,7 +26,7 @@ const App = () => {
   return (
     <div >
       <header className='bg-slate-800 w-full h-14 flex flex-col
-       text-center text-green-300 font-bold text-3xl sm:text-5xl  border-b-2 pb-3  border-slate-700'>
+       text-center text-green-300 font-bold text-3xl sm:text-5xl  border-b-2  border-slate-700'>
         <p className=' flex items-center flex-col my-auto'>Dad Joke's</p>
       </header>
       <div className='flex flex-col items-center justify-center py-11 sm:px-0 px-4 bg-slate-900 text-white shadow-2xl font-sans'>
@@ -29,7 +39,9 @@ const App = () => {
 
             <div className='flex font-mono sm:text-2xl text-xl font-semibold tracking-wider text-center gap-8'>
     
-            <h1 className='cursor-pointer sm:hover:text-gray-500' onClick={ () =>{handleToggle(index)} } key={index}> {item.question} </h1>
+            <h1 
+            // id='p'
+            className='cursor-pointer sm:hover:text-gray-500   duration-300 ' onClick={ () =>{handleToggle(index)} } key={index}> {item.question} </h1>
               <span onClick={ () =>{handleToggle(index)}}>
                 {isClicked === index ? <FiMinus color='green' /> : <FiPlus color='green'/>}
               </span>
@@ -44,7 +56,8 @@ const App = () => {
 
           { isClicked === index  ?(
             <h3
-             className='flex flex-col text-center font-sans text-xl font-semibold tracking-widest py-5 border-y-2 sm:bg-slate-900 bg-slate-800 border-emerald-500' 
+            // id='p'
+             className='flex flex-col text-center font-sans text-xl font-semibold tracking-widest py-5 border-y-2 sm:bg-slate-900 bg-slate-800 border-emerald-500 transition duration-1000 ease-in-out' 
              >
               {item.answer}
               </h3> 
--- src/components/Data.js
+++ src/components/Data.js
@@ -247,4 +247,307 @@ export const Data = [
   "question": "Why did the boy bring a ladder to the bar?",
   "answer": "He wanted to get a high stool."
   },
+  {
+    "question": "Why did the scarecrow win an award?",
+    "answer": "Because he was outstanding in his field."
+  },
+  {
+    "question": "Why did the tomato turn red?",
+    "answer": "Because it saw the salad dressing."
+  },
+  {
+    "question": "Why did the cookie go to the doctor?",
+    "answer": "Because it was feeling crumbly."
+  },
+  {
+    "question": "Why was the math book sad?",
+    "answer": "Because it had too many problems."
+  },
+  {
+    "question": "Why did the bicycle fall over?",
+    "answer": "Because it was two-tired."
+  },
+  {
+    "question": "Why did the chicken cross the playground?",
+    "answer": "To get to the other slide."
+  },
+  {
+    "question": "Why was the computer cold?",
+    "answer": "Because it left its Windows open."
+  },
+  {
+    "question": "Why did the frog call his insurance company?",
+    "answer": "He had a jump in his car."
+  },
+  {
+    "question": "Why did the hipster burn his tongue?",
+    "answer": "He drank his coffee before it was cool."
+  },
+  {
+    "question": "Why did the man put his money in the freezer?",
+    "answer": "He wanted cold hard cash."
+  },
+  {
+    "question": "Why did the musician call his best friend?",
+    "answer": "He wanted a bass player."
+  },
+  {
+    "question": "Why did the physicist cross the road?",
+    "answer": "To get to the other side."
+  },
+  {
+    "question": "Why did the police officer retire?",
+    "answer": "He felt he had served and protected."
+  },
+  {
+    "question": "Why did the vegetarian bring a ladder to the bar?",
+    "answer": "He wanted to reach the nuts."
+  },
+  {
+    "question": "Why did the tree go to the dentist?",
+    "answer": "To get a root canal."
+  },
+  {
+    "question": "Why did the duck go to the doctor?",
+    "answer": "He had the flu."
+  },
+  {
+    "question": "Why did the ball go to the doctor?",
+    "answer": "It had a bounce-ache."
+  },
+  {
+    "question": "Why did the snake go to school?",
+    "answer": "To learn his hiss-tory."
+  },
+  {
+    "question": "Why did the bank teller go to school?",
+    "answer": "To learn how to count money."
+  },
+  {
+    "question": "Why did the scarecrow win an award?",
+    "answer": "Because he was outstanding in his field."
+  },
+  {
+    "question": "Why did the moon go to school?",
+    "answer": "To learn how to light up the night."
+  },
+  {
+    "question": "Why did the cookie go to the doctor?",
+    "answer": "Because it was feeling crumbly"
+  },{
+    question: "Why did the scarecrow win an award?",
+    answer: "Because he was outstanding in his field."
+    },
+    {
+    question: "Why don't scientists trust atoms?",
+    answer: "Because they make up everything."
+    },
+    {
+    question: "Why did the bicycle fall over?",
+    answer: "Because it was two-tired."
+    },
+    {
+    question: "Why did the cookie go to the doctor?",
+    answer: "Because he felt crumbly."
+    },
+    {
+    question: "Why did the computer go to the doctor?",
+    answer: "Because it had a virus."
+    },
+    {
+    question: "Why did the frog call his insurance company?",
+    answer: "He had a jump in his car."
+    },
+    {
+    question: "Why did the man put his money in the freezer?",
+    answer: "He wanted cold hard cash."
+    },
+    {
+    question: "Why did the mushroom go to the party?",
+    answer: "Because he was a fun-gi."
+    },
+    {
+    question: "Why did the tomato turn red?",
+    answer: "Because it saw the salad dressing."
+    },
+    {
+    question: "Why did the man with one hand cross the road?",
+    answer: "To get to the second-hand store."
+    },
+    {
+    question: "Why did the man put his money in the blender?",
+    answer: "He wanted to make liquid assets."
+    },
+    {
+    question: "Why did the cookie go to the therapy session?",
+    answer: "Because he felt crumbly."
+    },
+    {
+    question: "Why did the man with a broken leg go to the bank?",
+    answer: "To get a loan."
+    },
+    {
+    question: "Why did the man with a broken arm go to the bar?",
+    answer: "To get a drink on the rocks."
+    },
+    {
+    question: "Why did the duck cross the playground?",
+    answer: "To get to the other slide."
+    },
+    {
+    question: "Why did the math book look so mad?",
+    answer: "Because it had too many problems."
+    },
+    {
+    question: "Why did the man put his money in the toaster?",
+    answer: "He wanted to have warm bread."
+    },
+    {
+    question: "Why did the man put his money in the oven?",
+    answer: "He wanted to bake some dough."
+    },
+    {
+    question: "Why did the man with a broken leg go to the beach?",
+    answer: "To get some sand in his cast."
+    },
+    {
+    question: "Why did the man with a broken arm go to the restaurant?",
+    answer: "To get a hand-out."
+    },
+    {
+    question: "Why did the man with a broken hand go to the jewelry store?",
+    answer: "To get a ring for his finger."
+    },
+    {
+    question: "Why did the man with a broken foot go to the store?",
+    answer: "To get a sole for his shoe."
+    },
+    {
+      question: "Why don't scientists trust atoms?",
+      answer: "Because they make up everything."
+    },
+    {
+      question: "Why was the math book sad?",
+      answer: "Because it had too many problems."
+    },
+    {
+      question: "Why did the scarecrow win an award?",
+      answer: "Because he was outstanding in his field."
+    },
+    {
+      question: "What did the grape say when it got stepped on?",
+      answer: "Nothing, it just let out a little wine."
+    },
+    {
+      question: "Why don't oysters share their pearls?",
+      answer: "Because they're shellfish."
+    },
+    {
+      question: "Why did the cookie go to the doctor?",
+      answer: "Because it felt crumbly."
+    },
+    {
+      question: "Why don't seagulls fly over the bay?",
+      answer: "Because then they'd be bagels."
+    },
+    {
+      question: "Why did the computer go to the doctor?",
+      answer: "Because it had a virus."
+    },
+    {
+      question: "Why did the bicycle fall over?",
+      answer: "Because it was two-tired."
+    },
+    {
+      question: "Why was the math book sad?",
+      answer: "Because it had too many problems."
+    },
+    {
+      question: "Why did the tomato turn red?",
+      answer: "Because it saw the salad dressing."
+    },
+    {
+      question: "Why don't dinosaurs drive cars?",
+      answer: "Because they're extinct."
+    },
+    {
+      question: "Why did the chicken cross the playground?",
+      answer: "To get to the other slide."
+    },
+    {
+      question: "Why did the banana go to the doctor?",
+      answer: "Because it wasn't peeling too good."
+    },
+    {
+      question: "Why don't ghosts have shadows?",
+      answer: "Because they're transparent."
+    },
+    {
+      question: "Why did the cookie go to the doctor?",
+      answer: "Because it felt crumbly."
+    },
+    {
+      question: "Why did the math book look so mad?",
+      answer: "Because it had too many problems."
+    },
+    {
+      question: "Why was the calendar popular?",
+      answer: "Because it had a lot of dates."
+    },
+    {
+      question: "Why don't seagulls fly over the bay?",
+      answer: "Because then they'd be bagels."
+    },
+    {
+      question: "Why did the bicycle fall over?",
+      answer: "Because it was two-tired."
+    },
+    {
+      question: "Why did the scarecrow win an award?",
+      answer: "Because he was outstanding in his field."
+    },
+    {
+      question: "Why don't scientists trust atoms?",
+      answer: "Because they make up everything."
+    },
+    {
+      question: "Why don't ghosts have shadows?",
+      answer: "Because they're transparent."
+    },
+    {
+      "question": "Why did the scarecrow win an award?",
+      "answer": "Because he was outstanding in his field."
+    },
+    {
+      "question": "Why do birds fly south for the winter?",
+      "answer": "Because it's too far to walk."
+    },
+    {
+      "question": "Why did the cookie go to the doctor?",
+      "answer": "Because he felt crumbly."
+    },
+    {
+      "question": "Why did the tomato turn red?",
+      "answer": "Because it saw the salad dressing."
+    },
+    {
+      "question": "Why did the hipster burn his tongue?",
+      "answer": "He drank his coffee before it was cool."
+    },
+    {
+      "question": "Why did the dog cross the road?",
+      "answer": "To get to the barking lot."
+    },
+    {
+      "question": "Why did the bicycle fall over?",
+      "answer": "Because it was two-tired."
+    },
+    {
+      "question": "Why did the computer go to the doctor?",
+      "answer": "Because it had a virus."
+    },
+    {
+      "question": "Why did the math book look so mad?",
+      "answer": "Because it had too many problems."
+    },
 ]
